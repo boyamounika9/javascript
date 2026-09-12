3784a164ae73f1c5db7c84bf3969f4a45eaa547c
mounika <189mounika.b@gmail.com>
2026-09-12 16:42:43 +0530

    console.log(undefined===undefined) console.log(undefined==undefined) console.log(null===null) console.log(null==null) console.log({}==={}) console.log([]===[])

---
+++ jsprogramming/test1.js
@@ -0,0 +1,14 @@
+for(let i;i<=5;i++){
+console.log(i)
+}
+
+console.log(undefined===undefined)
+console.log(undefined==undefined)
+console.log(null===null)
+console.log(null==null)
+console.log({}==={})
+console.log([]===[])
+
+
+
+
